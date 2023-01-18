fe2607ea698e0902e7b461f9613e97c4ddabbf14
Максим Богащенко <sergeyivanov85@yandex.ru>
2023-01-18 19:50:55 +0300

    Добавьте файлы проекта.

---
+++ FormLesson_2/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormLesson_2 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            Timer timer = new Timer();
+            timer.Interval = 500000;
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e) {
+            this.Close();
+        }
+        public ComboBox ComboBox = new ComboBox();
+        private void button1_Click(object sender, EventArgs e) {
+            Form1 form1 = new Form1();
+            Form form = new Form();
+            Button button = new Button();
+            button.Text = "Кнопка";
+            button.Location = new Point(form.Width / 2, form.Height / 2);
+            ComboBox comboBox = new ComboBox();
+            comboBox.Location = new Point(form.Width / 2, form.Height / 2 - button.Height - 5);
+            ComboBox = comboBox;
+            form.Controls.Add(button);
+            form.Controls.Add(comboBox);
+            button.Click += Button_Click1;
+            button.Click += Button_Click;
+            
+            form1.Show();
+            void Button_Click1(object sender, EventArgs e) {
+                ComboBox.Items.Add("Третий");
+                Form2 form2 = new Form2();
+                form2.ShowDialog();
+
+            }
+            form.ShowDialog();
+
+        }
+
+       
+
+        private void Button_Click(object sender, EventArgs e) {
+            ComboBox.Items.Add("Первый");
+            ComboBox.Items.Add("Второй");
+            ComboBox.Text = "Выберите значение";
+            ComboBox.Text = ComboBox.Items[0].ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            this.Dispose();
+        }
+    }
+}
---
+++ FormLesson_2/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FormLesson_2 {
+    public enum ButClick {
+        Ok,
+        Cancel,
+        Help
+    }
+    public partial class Form2 : Form {
+        public Form2() {
+            InitializeComponent();
+        }
+        public ButClick ButClick = ButClick.Ok;
+        private void Form2_Load(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            ButClick = ButClick.Ok;
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            ButClick = ButClick.Cancel;
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            ButClick = ButClick.Help;
+            this.Close();
+        }
+    }
+}
